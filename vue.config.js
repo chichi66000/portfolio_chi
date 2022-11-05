d5310b9475924789c9c49e4fee1982bd7900344c
khanhchi <le.khanhchi@yahoo.fr>
2022-11-05 20:25:02 +0100

    add deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: "/portfolio_chi/"
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/porfolio_chi/'
+    : '/'
 })
